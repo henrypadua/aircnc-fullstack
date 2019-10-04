f0dc20ebdd3b5f58cc7b381a16d41300fbbf7fc4
henrypadua <henrybpj@live.com>
2019-10-04 01:10:19 -0300

    Tela de login, rotas configuradas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
 import './App.css';
+
+import logo from './assets/logo.svg';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <Routes />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+// import { Container } from './styles';
+
+export default function Login({ history }) {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+
+    history.push('/dashboard');
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  return (
+    <>
+      <p>
+        Ofereça <strong>spots</strong> para programadores e econtre{' '}
+        <strong>talentos</strong> para sua empresa
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Seu melhor e-mail"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <button className="btn" type="submit">
+          Entrar
+        </button>
+      </form>
+    </>
+  );
+}
